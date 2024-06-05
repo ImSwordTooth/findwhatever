7c70249e280832a0d205db858a653292e75633f8
lihu <lihu1@le.com>
2024-06-05 14:14:56 +0800

    frameList 改成数字之前

--- action.js
+++ action.js
@@ -1,6 +1,36 @@
+// isFrame = window !== window.top; // 运行此脚本的是否为 frame
+
 // 每次点击的时候才开始创建 dom 查找树，否则会 dom 节点过旧
 reCheckTree()
+chrome.storage.local.set({ frameList: [] })
 
+if (!isFrame) {
+    if (document.getElementById('searchWhateverPopup')) {
+        // 如果已存在相同的弹出窗口，将其移除
+        observer.disconnect()
+        CSS.highlights.clear();
+        document.getElementById('searchWhateverPopup').remove();
+    } else {
+        // 创建新的弹出窗口
+        createPopup();
+        start()
+        observer.observe(document.body, {
+            subtree: false, // 监听以 target 为根节点的整个子树。包括子树中所有节点的属性，而不仅仅是针对 target。
+            childList: true, // 监听 target 节点中发生的节点的新增与删除（同时，如果 subtree 为 true，会针对整个子树生效）。
+            attributes: false, // 不监听属性值
+            characterData: true // 监听声明的 target 节点上所有字符的变化。
+        })
+    }
+
+} else {
+    start()
+    observer.observe(document.body, {
+        subtree: false, // 监听以 target 为根节点的整个子树。包括子树中所有节点的属性，而不仅仅是针对 target。
+        childList: true, // 监听 target 节点中发生的节点的新增与删除（同时，如果 subtree 为 true，会针对整个子树生效）。
+        attributes: false, // 不监听属性值
+        characterData: true // 监听声明的 target 节点上所有字符的变化。
+    })
+}
 
 // 创建一个自定义的弹出窗口
 function createPopup() {
@@ -58,23 +88,6 @@ function createPopup() {
 
     // 插入到页面
     document.body.appendChild(popup);
-}
-
-if (document.getElementById('searchWhateverPopup')) {
-    // 如果已存在相同的弹出窗口，将其移除
-    observer.disconnect()
-    CSS.highlights.clear();
-    document.getElementById('searchWhateverPopup').remove();
-} else {
-    // 创建新的弹出窗口
-    createPopup();
-    start()
-    observer.observe(document.body, {
-        subtree: false, // 监听以 target 为根节点的整个子树。包括子树中所有节点的属性，而不仅仅是针对 target。
-        childList: true, // 监听 target 节点中发生的节点的新增与删除（同时，如果 subtree 为 true，会针对整个子树生效）。
-        attributes: false, // 不监听属性值
-        characterData: true // 监听声明的 target 节点上所有字符的变化。
-    })
 }
 
 
--- popContent.js
+++ popContent.js
@@ -1,10 +1,20 @@
-let treeWalker, allNodes, currentNode, setting = {}, activeHighlightIndex = 0, filteredRangeList = [], rangesFlat;
+let isFrame,
+    treeWalker,
+    allNodes,
+    currentNode,
+    setting = {},
+    activeHighlightIndex = 0,
+    filteredRangeList = [],
+    rangesFlat;
+
+isFrame = window !== window.top;
 
 const observer = new MutationObserver(() => {
     reCheckTree();
     doSearch();
 })
 
+// 生成匹配节点树
 const reCheckTree = () => {
     treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, (node) => {
         // 父元素是 svg、script、script 的时候，不置入范围
@@ -31,148 +41,123 @@ const start = async () => {
 
     setting = await chrome.storage.sync.get(['searchValue', 'isMatchCase', 'isWord', 'isReg']); // 缓存值
 
-    if (setting.searchValue) {
-        searchInput.value = setting.searchValue;
-    }
-    if (setting.isMatchCase) {
-        matchCaseButton.classList.add('active')
-    }
-    if (setting.isWord) {
-        wordButton.classList.add('active')
-    }
-    if (setting.isReg) {
-        regButton.classList.add('active')
-    }
+    // 仅在主界面进行一些 dom 的操作，frame 内的只搜索
+    if (!isFrame) {
+        // 获取用户选中的文本，有就立即填进去
+        const selection = window.getSelection().toString();
+        if (selection) {
+            searchInput.value = selection;
+            chrome.storage.sync.set({ searchValue: selection })
+            setting.searchValue = selection;
+        } else if (setting.searchValue) {
+            searchInput.value = setting.searchValue;
+        }
+        if (setting.isMatchCase) {
+            matchCaseButton.classList.add('active')
+        }
+        if (setting.isWord) {
+            wordButton.classList.add('active')
+        }
+        if (setting.isReg) {
+            regButton.classList.add('active')
+        }
 
-    searchInput.oninput = (e) => {
-        const value = e.target.value.trim();
+        searchInput.oninput = async (e) => {
+            const value = e.target.value.trim();
+            // 向背景脚本发送消息以获取当前标签信息
+            await chrome.storage.sync.set({ searchValue: value })
+            await chrome.storage.local.set({ frameList: [] })
+            setting.searchValue = value;
+            // 主界面执行完搜索之后，通知 frame 可以搜索了
+            await doSearch()
+            chrome.storage.local.set({ searchInFrame: Math.random() })
+        }
 
-        // 向背景脚本发送消息以获取当前标签信息
-        chrome?.runtime?.sendMessage({
-            action: 'saveData',
-            data: {
-                searchValue: value
+
+        matchCaseButton.onclick = async (e) => {
+            const { classList } = e.currentTarget
+            const isActive = classList.contains('active')
+            if (isActive) {
+                classList.remove('active')
+            } else {
+                classList.add('active')
             }
-        });
-
-        setting.searchValue = value;
-        doSearch()
-    }
-
-    document.querySelector('#searchWhateverPopup #searchwhatever_result .prev').onclick = () => {
-        if (activeHighlightIndex > 1) {
-            activeHighlightIndex--;
-        } else {
-            activeHighlightIndex = rangesFlat.length;
-        }
-        observer.disconnect()
-
-        document.querySelector('#searchWhateverPopup #searchwhatever_result .current').innerText = activeHighlightIndex;
-        filteredRangeList[activeHighlightIndex - 1].scrollIntoView({ behavior: 'instant', block: 'center' })
-        observer.observe(document.body, {
-            subtree: false, // 监听以 target 为根节点的整个子树。包括子树中所有节点的属性，而不仅仅是针对 target。
-            childList: true, // 监听 target 节点中发生的节点的新增与删除（同时，如果 subtree 为 true，会针对整个子树生效）。
-            attributes: false, // 不监听属性值
-            characterData: true // 监听声明的 target 节点上所有字符的变化。
-        })
-        CSS.highlights.set('search-results-active', new Highlight(rangesFlat[activeHighlightIndex - 1]))
-    }
-
-    document.querySelector('#searchWhateverPopup #searchwhatever_result .next').onclick = () => {
-        if (activeHighlightIndex < rangesFlat.length) {
-            activeHighlightIndex++;
-        } else {
-            activeHighlightIndex = 1;
-        }
-        observer.disconnect()
-
-        document.querySelector('#searchWhateverPopup #searchwhatever_result .current').innerText = activeHighlightIndex;
-        filteredRangeList[activeHighlightIndex - 1].scrollIntoView({ behavior: 'instant', block: 'center' })
-        observer.observe(document.body, {
-            subtree: false, // 监听以 target 为根节点的整个子树。包括子树中所有节点的属性，而不仅仅是针对 target。
-            childList: true, // 监听 target 节点中发生的节点的新增与删除（同时，如果 subtree 为 true，会针对整个子树生效）。
-            attributes: false, // 不监听属性值
-            characterData: true // 监听声明的 target 节点上所有字符的变化。
-        })
-        CSS.highlights.set('search-results-active', new Highlight(rangesFlat[activeHighlightIndex - 1]))
-    }
-
-    matchCaseButton.addEventListener('click', (e) => {
-        const { classList } = e.currentTarget
-        const isActive = classList.contains('active')
-        if (isActive) {
-            classList.remove('active')
-            chrome.runtime.sendMessage({
-                action: 'saveData',
-                data: {
-                    isMatchCase: false
-                }
-            });
-            setting.isMatchCase = false
-        } else {
-            classList.add('active')
-            chrome.runtime.sendMessage({
-                action: 'saveData',
-                data: {
-                    isMatchCase: true
-                }
-            });
-            setting.isMatchCase = true
+            await chrome.storage.sync.set({ isMatchCase: !isActive })
+            await chrome.storage.local.set({ frameList: [] })
+            setting.isMatchCase = !isActive
+            await doSearch()
+            chrome.storage.local.set({ searchInFrame: Math.random() })
         }
 
-        doSearch()
-    })
+        wordButton.onclick = async (e) => {
+            const { classList } = e.currentTarget
+            const isActive = classList.contains('active')
+            if (isActive) {
+                classList.remove('active')
+            } else {
+                classList.add('active')
+            }
 
-    wordButton.onclick = (e) => {
-        const { classList } = e.currentTarget
-        const isActive = classList.contains('active')
-        if (isActive) {
-            classList.remove('active')
-            chrome.runtime.sendMessage({
-                action: 'saveData',
-                data: {
-                    isWord: false
-                }
-            });
-            setting.isWord = false
-        } else {
-            classList.add('active')
-            chrome.runtime.sendMessage({
-                action: 'saveData',
-                data: {
-                    isWord: true
-                }
-            });
-            setting.isWord = true
+            await chrome.storage.sync.set({ isWord: !isActive })
+            await chrome.storage.local.set({ frameList: [] })
+            setting.isWord = !isActive
+            await doSearch()
+            chrome.storage.local.set({ searchInFrame: Math.random() })
         }
 
-        doSearch()
-    }
+        regButton.onclick = async (e) => {
+            const { classList } = e.currentTarget
+            const isActive = classList.contains('active')
+            if (isActive) {
+                classList.remove('active')
+            } else {
+                classList.add('active')
+            }
 
-    regButton.onclick = (e) => {
-        const { classList } = e.currentTarget
-        const isActive = classList.contains('active')
-        if (isActive) {
-            classList.remove('active')
-            chrome.runtime.sendMessage({
-                action: 'saveData',
-                data: {
-                    isReg: false
-                }
-            });
-            setting.isReg = false
-        } else {
-            classList.add('active')
-            chrome.runtime.sendMessage({
-                action: 'saveData',
-                data: {
-                    isReg: true
-                }
-            });
-            setting.isReg = true
+            await chrome.storage.sync.set({ isReg: !isActive })
+            await chrome.storage.local.set({ frameList: [] })
+            setting.isReg = !isActive
+            await doSearch()
+            chrome.storage.local.set({ searchInFrame: Math.random() })
         }
 
-        doSearch()
+        document.querySelector('#searchWhateverPopup #searchwhatever_result .prev').onclick = () => {
+            if (activeHighlightIndex > 1) {
+                activeHighlightIndex--;
+            } else {
+                activeHighlightIndex = rangesFlat.length;
+            }
+            observer.disconnect()
+
+            document.querySelector('#searchWhateverPopup #searchwhatever_result .current').innerText = activeHighlightIndex;
+            filteredRangeList[activeHighlightIndex - 1].scrollIntoView({ behavior: 'instant', block: 'center' })
+            observer.observe(document.body, {
+                subtree: false, // 监听以 target 为根节点的整个子树。包括子树中所有节点的属性，而不仅仅是针对 target。
+                childList: true, // 监听 target 节点中发生的节点的新增与删除（同时，如果 subtree 为 true，会针对整个子树生效）。
+                attributes: false, // 不监听属性值
+                characterData: true // 监听声明的 target 节点上所有字符的变化。
+            })
+            CSS.highlights.set('search-results-active', new Highlight(rangesFlat[activeHighlightIndex - 1]))
+        }
+
+        document.querySelector('#searchWhateverPopup #searchwhatever_result .next').onclick = () => {
+            if (activeHighlightIndex < rangesFlat.length) {
+                activeHighlightIndex++;
+            } else {
+                activeHighlightIndex = 1;
+            }
+            observer.disconnect()
+
+            document.querySelector('#searchWhateverPopup #searchwhatever_result .current').innerText = activeHighlightIndex;
+            filteredRangeList[activeHighlightIndex - 1].scrollIntoView({ behavior: 'instant', block: 'center' })
+            observer.observe(document.body, {
+                subtree: false, // 监听以 target 为根节点的整个子树。包括子树中所有节点的属性，而不仅仅是针对 target。
+                childList: true, // 监听 target 节点中发生的节点的新增与删除（同时，如果 subtree 为 true，会针对整个子树生效）。
+                attributes: false, // 不监听属性值
+                characterData: true // 监听声明的 target 节点上所有字符的变化。
+            })
+            CSS.highlights.set('search-results-active', new Highlight(rangesFlat[activeHighlightIndex - 1]))
+        }
     }
 
     // 启动后立即进行一次搜索
@@ -181,15 +166,6 @@ const start = async () => {
 
 async function doSearch() {
     const { isMatchCase, searchValue, isWord, isReg } = setting
-
-    if (!CSS.highlights) {
-
-        window.postMessage({
-            type: '__find_whatever__searchChange__back__',
-            value: '不支持 CSS highlights'
-        })
-        return;
-    }
 
     CSS.highlights.clear();
 
@@ -206,34 +182,27 @@ async function doSearch() {
 
         let regContent = searchValue
         if (!isReg) {
-            regContent = regContent.replace(/([^a-z0-9_ \n])/g, '\\$1')
+            regContent = regContent.replace(/([^a-zA-Z0-9_ \n])/g, '\\$1')
         }
         if (isWord) {
             regContent = `\\b${regContent}\\b`
         }
         let execResLength = searchValue.value; // 匹配结果的长度，一般情况下等于字符长度，如果是正则，就得是正则结果的长度
         const reg = new RegExp(regContent, `${isMatchCase ? '' : 'i'}dg`);
+
         while (startPos < text.length) {
             let index;
-
-            let res;
-            if (isMatchCase) {
-                res = reg.exec(text.substring(startPos))
-            } else {
-                res = reg.exec(text.substring(startPos).toLowerCase())
-            }
+            const res = reg.exec(text.substring(startPos));
 
             if (res) {
                 index = res.indices[0][0]
                 execResLength = res.indices[0][1] - res.indices[0][0]
             } else {
-                index = -1
-            }
-
-            if (index === -1) {
+                index = -1;
                 break;
             }
-            indices.push(index);
+
+            indices.push(startPos + index);
             startPos += index + execResLength;
         }
 
@@ -246,16 +215,11 @@ async function doSearch() {
         })
     }).flat()
 
-
     const searchResultsHighlight = new Highlight(...rangesFlat)
 
     // 先断开再连接，否则会引起观察者的变动，导致无限循环
     observer.disconnect()
-    document.querySelector('#searchWhateverPopup #searchwhatever_result .total').innerText = searchResultsHighlight.size;
-    if (searchResultsHighlight.size > 0) {
-        activeHighlightIndex = 1
-        document.querySelector('#searchWhateverPopup #searchwhatever_result .current').innerText = activeHighlightIndex;
-    }
+    // document.querySelector('#searchWhateverPopup #searchwhatever_result .total').innerText = searchResultsHighlight.size;
     observer.observe(document.body, {
         subtree: false, // 监听以 target 为根节点的整个子树。包括子树中所有节点的属性，而不仅仅是针对 target。
         childList: true, // 监听 target 节点中发生的节点的新增与删除（同时，如果 subtree 为 true，会针对整个子树生效）。
@@ -263,5 +227,36 @@ async function doSearch() {
         characterData: true // 监听声明的 target 节点上所有字符的变化。
     })
     CSS.highlights.set('search-results', searchResultsHighlight)
-    CSS.highlights.set('search-results-active', new Highlight(rangesFlat[activeHighlightIndex - 1]))
+    console.log(searchResultsHighlight)
+
+    let storageFrameList = (await chrome.storage.local.get(['frameList'])).frameList || [];
+    console.log({ storageFrameList })
+    if (!isFrame) {
+        storageFrameList.unshift(rangesFlat.length)
+    } else {
+        storageFrameList.push(rangesFlat.length)
+    }
+    await chrome.storage.local.set({ frameList: storageFrameList })
+
+    if (searchResultsHighlight.size > 0) {
+        activeHighlightIndex = 1
+        if (!isFrame) {
+            document.querySelector('#searchWhateverPopup #searchwhatever_result .current').innerText = activeHighlightIndex;
+        }
+        CSS.highlights.set('search-results-active', new Highlight(rangesFlat[activeHighlightIndex - 1]))
+    }
 }
+
+chrome.storage.onChanged.addListener(async (changes, areaName) => {
+
+    if(areaName === 'local') {
+        if (isFrame && changes.searchInFrame) {
+            setting = await chrome.storage.sync.get(['searchValue', 'isMatchCase', 'isWord', 'isReg']);
+            doSearch()
+        }
+        if (!isFrame && changes.frameList) {
+            console.log(changes)
+            document.querySelector('#searchWhateverPopup #searchwhatever_result .total').innerText = changes.frameList.newValue.reduce((a, b) => a+b, 0);
+        }
+    }
+})
