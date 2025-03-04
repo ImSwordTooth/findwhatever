079dd2b27f8bac11ad1e5574ea9d0f6e52265e29
lihu <lihu1@le.com>
2025-03-04 10:29:05 +0800

    修复 selection 失效bug

--- src/pages/Background/action.js
+++ src/pages/Background/action.js
@@ -15,15 +15,15 @@ window.handleCloseByEsc = (e) => {
         if (document.getElementById('__swe_container')) {
             closePop()
         } else {
+			const selection = window.getSelection().toString()
+			if (selection) {
+				await chrome.storage.sync.set({ searchValue: window.getSelection().toString() });
+			}
             // 创建新的弹出窗口
 			createOrUpdatePopup();
             await chrome?.runtime?.sendMessage({ // chrome.scripting 只能在 background.js 里使用，所以不直接在这写了
                 action: 'openAction'
             });
-            const selection = window.getSelection().toString()
-            if (selection) {
-                await chrome.storage.sync.set({ searchValue: window.getSelection().toString() });
-            }
 			document.addEventListener('keydown', window.handleCloseByEsc)
         }
     } else {
